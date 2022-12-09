ccec790803942742ab8bb958c163a1402e3e54fd
AgenTwilight <mhmmad.iqbal75@gmail.com>
2022-12-09 08:26:09 -0500

    Praktikum 10

---
+++ H071221087/Praktikum 10/TP10_2_H071221087.py
@@ -0,0 +1,13 @@
+class Cheetah:
+    def kecepatan(self):
+        return 'Cheetah hewan paling cepat di dunia'
+
+class Siput:
+    def kecepatan(self):
+        return 'Kukang hewan paling lambat didunia'
+
+hewan1 = Cheetah()
+hewan2 = Siput()
+
+print(hewan1.kecepatan())
+print(hewan2.kecepatan())
