97933fb969ffbb60b441c0ee00c2adeda6aea2e7
IM4MS4M4D <yungramen303@gmail.com>
2022-12-10 16:04:51 -0800

    Tugas 7

---
+++ H071221084/Prakitkum 7/TP7_1_H071221084.py
@@ -0,0 +1,11 @@
+import re
+
+try:
+    string_s = input("masukkan String S: ")
+    pattern = re.fullmatch("[2468a-zA-Z]{40}[13579\s]{5}", string_s)
+    if pattern :
+        print("True")
+    else:
+        print("False")
+except:
+    print("False")
